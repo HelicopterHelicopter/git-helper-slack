6f795615822dcf0c5c1c1acc0a9069f14ecd451e
jheel-phantom <jhljheel@gmail.com>
2024-07-26 14:47:18 +0530

    added filemanager to read one text while and serve it as context for the model

--- index.js
+++ index.js
@@ -1,23 +1,13 @@
 const { App } = require('@slack/bolt');
 require("dotenv").config();
 const axios = require('axios');
-const {Client} = require("@elastic/elasticsearch");
-const {GoogleGenerativeAI} = require("@google/generative-ai");
+const { GoogleGenerativeAI } = require("@google/generative-ai");
 const fs = require('fs');
+const { GoogleAIFileManager } = require('@google/generative-ai/server');
 const genai = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genai.getGenerativeModel({model: "gemini-1.5-flash"});
+const model = genai.getGenerativeModel({ model: "gemini-1.5-pro" });
 
-const client = new Client({
-    auth:{
-        username:process.env.ELASTIC_USERNAME,
-        password:process.env.ELASTIC_PASSWORD
-    },
-    node:"https://localhost:9200",
-    tls:{
-        ca: fs.readFileSync("http_ca.crt"),
-        rejectUnauthorized:false
-    }
-})
+
 
 const app = new App({
     token: process.env.SLACK_BOT_TOKEN,
@@ -26,34 +16,33 @@ const app = new App({
     appToken: process.env.SLACK_APP_TOKEN
 });
 
+const fileManager = new GoogleAIFileManager(process.env.GEMINI_API_KEY);
+
+let uploadResult;
+fileManager.uploadFile("farmartapp-backend.txt", {
+    mimeType: "text/plain",
+    displayName: "farmart-app-backend.txt",
+}).then((result) => {
+    console.log(result);
+    uploadResult = result;
+}).catch((error) => {
+    console.error(error);
+});
+
+
 const elasticsearch_url = "https://localhost:9200/farmart-app-backend/_search"
 
 app.message(async ({ message, say }) => {
     try {
-        const elasticSearchMessage = message.text.replaceAll("what","").replaceAll("the","");
-        const elasticRes = await client.search({
-            index:"farmart-app-backend",
-            query:{
-                multi_match:{
-                    query:elasticSearchMessage,
-                    fields:["data","file_name","path"]
-                }
-            },
-            size:10
-        })
 
-        const prompt_data = elasticRes.hits.hits.map((hit)=>{
-            return JSON.stringify({
-                "file_name":hit._source.file_name,
-                "path":hit._source.path,
-                "data":hit._source.data
-            });
-        })
-        console.log(prompt_data);
-
-        const prompt = `You are an AI assistant that answers user's queries about a github repo. The code is in javascript. You will be provided with relevant files data in an array of string which is a json object in strified format has the file content. Here is the relevant data ${prompt_data} \n Refer the above data and answer the follow query: ${message.text}`;
+        const prompt = `You are an AI assistant that answers user's queries about a github repo. The code is in javascript. The codebase is attached as a text file. \n Refer the codebase and answer the follow query: ${message.text}`;
         console.log(prompt);
-        const result = await model.generateContent(prompt);
+        const result = await model.generateContent([prompt, {
+            fileData: {
+                fileUri: uploadResult.file.uri,
+                mimeType: uploadResult.file.mimeType
+            }
+        }]);
         const response = await result.response;
 
         const text = response.text();
